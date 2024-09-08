319bdfeb729def86d106d0037af56786443e42c6
池下克彦 <katuhiko.ikesita@gmail.com>
2024-09-08 18:45:09 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import meow from 'meow';
 import type { Dirent } from 'fs';
-import { readdir, rename } from 'fs/promises';
+import { readdir, rename, stat } from 'fs/promises';
 import { resolve } from 'path';
 import { normalize } from './hfsDecomposition.js';
 
@@ -24,10 +24,16 @@ Usage
 const { root } = cli.flags;
 
 async function normalizeRecursively(path: string) {
-  const dir = await readdir(path, {
-    encoding: 'utf-8',
-    withFileTypes: true,
-  });
+  let dir: Dirent[];
+  try {
+    dir = await readdir(path, {
+      encoding: 'utf-8',
+      withFileTypes: true,
+    });
+  } catch (e) {
+    console.log(`Error in readdir ${path}`, e);
+    dir = [];
+  }
   const normalizeMap: Map<string, Dirent[]> = new Map();
   for (const dirent of dir) {
     const normalizedName = normalize(dirent.name);
@@ -39,19 +45,31 @@ async function normalizeRecursively(path: string) {
     dirents.push(dirent);
   }
   await Promise.all(
-    [...normalizeMap].flatMap(([normalizedName, dirents]): Promise<void>[] => {
+    [...normalizeMap].map(async ([normalizedName, dirents]): Promise<void> => {
       if (dirents.length > 1) {
         console.log(`${path} contains ${dirents.map((dirent) => dirent.name)}`);
         // TODO merge directories
-        if (dirents.some((x) => x.name === normalizedName)) {
-          return [];
+        if (dirents.some((x) => !x.isFile())) {
+          return;
+        }
+        const statsArr = await Promise.all(dirents.map((dirent) => stat(resolve(path, dirent.name))));
+        const nonEmptyFileCount = statsArr.reduce((count, stats) => count + (stats.size > 0 ? 1 : 0), 0);
+        if (nonEmptyFileCount > 1) {
+          console.log('conflict');
+          return;
+        }
+        if (nonEmptyFileCount === 0) {
         }
       }
       const rawName = dirents[0].name;
       if (rawName === normalizedName) {
-        return [];
+        return;
       }
-      return [rename(resolve(path, rawName), resolve(path, normalizedName))];
+      try {
+        await rename(resolve(path, rawName), resolve(path, normalizedName));
+      } catch (e) {
+        console.log(`Error in rename(${resolve(path, rawName)}, ${resolve(path, normalizedName)}):`, e);
+      }
     }),
   );
   for (const [normalizedName, [dirent]] of normalizeMap) {
